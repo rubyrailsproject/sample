d97657cdf9fe263b3906f2ff316a9a0c1329bc6a
rubyrailsproject <rubyrailsproject@gmail.com>
2011-10-17 13:14:02 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '978fbf57d54029d7eb32c3ff5a289328fab291538ba05963397000cfdc6f7590e8f5f77705a58a953e763ee5c8c018b7efc6ab0a0ac4260342286b86024d57bc'
