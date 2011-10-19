fecc9527ff85ab25d23e40743da57dff8eb45a44
rubyrailsproject <rubyrailsproject@gmail.com>
2011-10-19 10:36:35 +0100

    pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Sample::Application.routes.draw do
 
-  get "pages/contact"
-
   get "pages/home"
+
+  get "pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
