d7dc781e3cf91152e2dc9c9b684d51909df818c5
rubyrailsproject <rubyrailsproject@gmail.com>
2011-10-23 22:46:35 +0100

    Layouts and Links

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -48,5 +48,19 @@ describe PagesController do
                           "Ruby on Rails Tutorial Sample | About")
     end
   end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample | Help")
+    end
+  end
 end
 
