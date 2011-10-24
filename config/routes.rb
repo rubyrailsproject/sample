bddf0850971376c154a67d52f782bca9f679c19c
rubyrailsproject <rubyrailsproject@gmail.com>
2011-10-24 05:18:22 +0100

    Finished Layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Sample::Application.routes.draw do
 
+  get "users/new"
+
   root :to => "pages#home"
+
+  match '/signup',  :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
